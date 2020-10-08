3dcb051284a2a8ece2c61829b43d42d9724511aa
candacecodes <candacehchoe@gmail.com>
2020-10-08 13:12:53 -0700

    done.

---
+++ src/Flashcard.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function Flashcard({ flashcard }) {
+	const [flip, setFlip] = useState(false); // sets state for flip
+	const [height, setHeight] = useState("initial");
+
+	const frontEl = useRef();
+	const backEl = useRef();
+	//reference to any element we want that persists through refresh
+
+	function setMaxHeight() {
+		const frontHeight = frontEl.current.getBoundingClientRect().height;
+		const backHeight = frontEl.current.getBoundingClientRect().height;
+		setHeight(Math.max(frontHeight, backHeight, 100));
+	}
+
+	useEffect(setMaxHeight, [
+		flashcard.question,
+		flashcard.answer,
+		flashcard.options,
+	]);
+
+	useEffect(() => {
+		window.addEventListener("resize", setMaxHeight);
+		return () => window.removeEventListener("resize", setMaxHeight);
+	}, []);
+
+	return (
+		<div
+			className={`card ${flip ? "flip" : ""} `}
+			style={{ height: height }}
+			onClick={() => setFlip(!flip)}
+		>
+			<div className="front" ref={frontEl}>
+				{flashcard.question}
+				{flip ? flashcard.answer : flashcard.question}
+				<div className="flashcard-options">
+					{flashcard.options.map((option) => {
+						return <div className="flashcard-option">{option}</div>;
+					})}
+				</div>
+			</div>
+			<div className="back" ref={backEl}>
+				{flashcard.answer}
+			</div>
+		</div>
+	);
+}
